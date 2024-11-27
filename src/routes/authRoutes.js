2f7f02f25ddc35db6372ef6481e69ca01bc99601
Bellika <markusbellika@gmail.com>
2024-11-28 00:52:49 +0100

    added loginform and landingpage

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const { auth } = require('../middleware/auth')
 const { login } = require('../controllers/authController')
+
+router.get('/me', auth, (req, res) => {
+  res.json({ user: req.user})
+})
 
 router.post('/login', login)
 
--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5001
 connectDB()
 
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/protected', require('./routes/protectedRoutes'))
